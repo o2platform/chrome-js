d9d30f5999c0d65a56bc15f3b7b267b84e9a474d
Dinis Cruz <dinis.cruz@owasp.org>
2014-12-01 02:29:22 +0000

    more features and working repl

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-console.log('here')
-require('fluentnode')
+
+/*
+var coffee = require('coffee-script');
+var code = coffee._compileFile('./node_modules/fluentnode/src/fluent-array.coffee')
+var gui = require('nw.gui'); 
+gui.Window.get().eval(null,code)
+*/
+//[].add(12).first().log()
+//a = require('fluentnode');
+//var gui = require('nw.gui'); 
+//gui.Window.get().eval(null,"var extra =" + a.extra); extra()
+
+//var coffee = require('coffee-script')
+//console.log(coffee)
+
+//eval('./node_modules/fluentnode/node_modules/coffee-script/register');              // adding coffee-script support
+/*var code = coffee._compileFile('./node_modules/fluentnode/src/fluent-array.coffee')
+eval(console.log(code))
+Array.prototype.size = function() { return 'this is the size'}
+_array2 = [111,222]
+console.log(_array2)
+console.log(_array2.size())
+//console.log(a.str().keys)
 /*var win = nwgui.Window.get();
 win.showDevTools('', true);
 win.on("devtools-opened", function(url) {
